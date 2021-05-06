e9a31e7e8ee75432cb897f4a6753f3c04b14cb21
Davin09 <davinjason09@gmail.com>
2021-05-06 14:35:26 +0700

    commit on 'master'

---
+++ PBO/6-5-2021 PBO Inheritance 2.py
@@ -0,0 +1,24 @@
+#Parent Class
+class Person(object):
+
+    #__init__ is known as the constructor
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+#Child Class
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        #Invoking the __init__ of the parent class
+        Person.__init__(self, name, idnumber)
+
+#Creation of an object variable or an instance
+a = Employee('Rahul', 886012, 200000, "Intern")
+
+#Calling a function of the class Person using its instance
+a.display()
